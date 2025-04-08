46b22291f10a05ab265ea6276ba7799e8e7362f7
rajveer-s <rajsidhu2924@gmail.com>
2025-04-08 10:09:36 -0700

    blog list page made

--- src/pages/BlogList.jsx
+++ src/pages/BlogList.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+// src/pages/BlogList.jsx
+import React from "react";
+import { Link } from "react-router-dom";
+import { posts } from "../data/posts";
+import {motion} from "framer-motion";
+import useTitle from "../components/UseTitle";
 
-const BlogList = () => {
+function BlogList() {
+
+    useTitle("My Posts");
+
+
   return (
-    <div>BlogList</div>
-  )
+    <motion.main
+    className="container"
+    initial={{ opacity: 0, y: 50 }}         // Start slightly below, hidden
+    whileInView={{ opacity: 1, y: 0 }}      // Animate to fully visible
+    transition={{ duration: 1.2 }}
+    viewport={{ once: true }}               // Animate only once
+    >
+      <h1>All Posts</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.slug} style={{ marginBottom: "1rem" }}>
+            <Link to={`/post/${post.slug}`}>{post.title}</Link>
+            <br />
+            <span style={{ fontSize: "0.9rem", color: "#999" }}>
+              {post.date}
+            </span>
+          </li>
+        ))}
+      </ul>
+      <Link to="/" className="back-link">
+        ← Back to Home
+      </Link>
+    </motion.main>
+  );
 }
 
-export default BlogList
+export default BlogList;
